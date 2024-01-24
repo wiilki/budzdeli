df8878206762a262daa2ce076a5abbb010731753
Will <willkim0608@gmail.com>
2024-01-24 03:42:28 -0800

    Convert to express project

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const router = require('express').Router();
+
+// "/about" responds with the about.html file
+router.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/about.html'));
+});
+
+// "/contact" responds with the contact.html file
+router.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/contact.html'));
+});
+
+// "/shop-la" responds with the shop-la.html file
+router.get('/shop-la', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/shop-la.html'));
+});
+
+// "/shop-sfv" responds with the shop-sfv.html file
+router.get('/shop-sfv', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/shop-sfv.html'));
+});
+
+// All other routes respond with the index.html file
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// Initialize the app and create a port
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up body parsing, static, and route middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    console.log('Request URL:', req.originalUrl);
+    next();
+  });
+ 
+
+app.use(express.static('public'));
+app.use('/', htmlRoutes);
+
+// Start the server on the port
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
