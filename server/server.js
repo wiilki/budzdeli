084b9616047ae36e2b3d1700c6f0b1851caa8dc9
Will <willkim0608@gmail.com>
2024-01-28 03:08:41 -0800

    test

--- server/server.js
+++ server/server.js
@@ -3,10 +3,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Serve static files from the React app in production
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // Routes for ShopLA and ShopSFV
 const shopLARoute = require('./routes/shopLA');
@@ -17,12 +15,7 @@ app.use('/shopsfv', shopSFVRoute);
 
 // Catch-all handler to serve the React app for all other routes
 app.get('*', (req, res) => {
-    // Ensure that the environment is production before serving the build files
-    if (process.env.NODE_ENV === 'production') {
-        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-    } else {
-        res.send("App running in development mode");
-    }
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 // Start the server
