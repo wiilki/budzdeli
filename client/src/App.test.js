0c2f513b344b1588d48112f47fb4f6cc3623d05a
Will <willkim0608@gmail.com>
2024-01-28 02:08:09 -0800

    Revert

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';/* src/index.css or src/App.css */
-
-
+import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
