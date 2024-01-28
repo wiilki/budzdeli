df07ebb88afe5ee98e36dffdffea2d9170abb177
Will <willkim0608@gmail.com>
2024-01-28 02:48:46 -0800

    Install missing Babel plugin and update dependencies

--- client/src/components/pages/ShopSFV/ShopSFV.js
+++ client/src/components/pages/ShopSFV/ShopSFV.js
@@ -1,28 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './ShopSFV.css';
 
 function ShopSFV() {
-    const [key, setKey] = useState(0);
 
-    useEffect(() => {
-        // Create script element and set its properties
-        const script = document.createElement('script');
-        script.src = 'https://labudzdeli.wm.store/static/js/embed.js';
-
-        // Append the script to the body
-        document.body.appendChild(script);
-
-        // Trigger a rerender by updating the key state
-        setKey(prevKey => prevKey + 1);
-
-        // Clean up: remove the script when the component unmounts
-        return () => {
-            document.body.removeChild(script);
-        };
-    }, []); // Empty dependency array ensures this runs only once when component mounts
 
     return (
-        <section key={key} id="shop-sfv" className="shop-sfv">
+        <section id="shop-sfv" className="shop-sfv">
             <h1>SHOP SFV PAGE</h1>
         </section>
     );
