ac0d2a13a0a5f8da10e9c263c2bee83f6739eac1
Will <willkim0608@gmail.com>
2024-01-28 06:47:15 -0800

    test

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 // Serve static files (including CSS) from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './views')));
 
 // Routes for ShopLA and ShopSFV
 const shopLARoute = require('./routes/shopLA');
