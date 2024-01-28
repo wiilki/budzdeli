50357d810d30323ae37dba64be5da165bfff707c
Will <willkim0608@gmail.com>
2024-01-28 02:39:56 -0800

    Test React import

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App';/* src/index.css or src/App.css */
+
+
 
 test('renders learn react link', () => {
   render(<App />);
